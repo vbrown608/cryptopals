b9f57e6accdf3d70b72e0fa19c115485ecdc2dae
Vivian Brown <vivian@eff.org>
2019-04-02 21:37:05 -0700

    Challenge 11

--- main.go
+++ main.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 func main() {
+}
+
+func encryptionOracle(pt []byte) []byte {
+	// Generate a random key.
+	key := make([]byte, 16)
+	rand.Read(key)
+	frontPad := make([]byte, randInt(6)+5)
+
+	// Append 5-10 random bytes to front and back.
+	rand.Read(frontPad)
+	pt = append(frontPad, pt...)
+	backPad := make([]byte, randInt(6)+5)
+	rand.Read(backPad)
+	pt = append(pt, backPad...)
+
+	// Choose a random cipher.
+	cipher := randInt(2)
+	if cipher == 0 {
+		return aes128ECB(pt, key)
+	} else {
+		iv := make([]byte, 16)
+		rand.Read(iv)
+		return encryptCBC(pt, iv, key)
+	}
+}
+
+func randInt(max int) int {
+	i, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	if err != nil {
+		panic(err)
+	}
+	return int(i.Int64())
+}
+
+func detectOracleCipher(pt, ct []byte) int {
+	return countRepeats(ct)
 }
 
 // Implements PKCS#7 padding
--- main_test.go
+++ main_test.go
@@ -1,11 +1,23 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
 
 // Challenge 9
-func testPad(t *testing.T) {
+func TestPad(t *testing.T) {
 	padded := pad([]byte("YELLOW SUBMARINE"), 20)
 	if string(padded) != "YELLOW SUBMARINE\x04\x04\x04\x04" {
 		t.Error(padded)
 	}
+}
+
+func TestDetectOracleCipher(t *testing.T) {
+	pt, err := ioutil.ReadFile("files/11.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ct := encryptionOracle(pt)
+	detectOracleCipher(pt, ct)
 }
